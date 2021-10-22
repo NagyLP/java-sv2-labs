ee41263b45aadef6b165f6a5a5e0ee902964ee85
NagyLP <nagy.peter@pr.hu>
2021-10-22 13:16:10 +0200

    GyF-2.8.: Városok;Autómárka

---
+++ training-solutions/src/main/java/linebreak/Car.java
@@ -0,0 +1,12 @@
+package linebreak;
+
+public class Car {
+        public String getBrandAndTypeInSperarateLines(String brand, String type) {
+            return brand +"\n"+ type ;}
+
+    public static void main(String[] args) {
+            Car car = new Car();
+            System.out.println(car.getBrandAndTypeInSperarateLines("Aston Martin", "Heritage"));
+    }
+}
+
