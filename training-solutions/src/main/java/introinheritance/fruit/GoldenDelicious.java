5351c9de0147fd6cf00d2bd742d54e1fa2812f86
NagyLP <nagy.peter@pr.hu>
2022-01-03 18:03:19 +0100

    GYF-14.: Öröklődés

---
+++ training-solutions/src/main/java/introinheritance/fruit/GoldenDelicious.java
@@ -0,0 +1,10 @@
+package introinheritance.fruit;
+
+public class GoldenDelicious extends Apple {
+
+    private final Colour colour = Colour.YELLOW;
+
+    public Colour getColour() {
+        return colour;
+    }
+}
