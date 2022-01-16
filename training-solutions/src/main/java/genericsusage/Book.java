8b8c192480670474c781cc0e6ebe39716e91a1b2
NagyLP <nagy.peter@pr.hu>
2022-01-16 22:34:08 +0100

    Haladó: Generikusok

---
+++ training-solutions/src/main/java/genericsusage/Book.java
@@ -0,0 +1,14 @@
+package genericsusage;
+
+public class Book {
+
+    String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ training-solutions/src/main/java/genericsusage/withoutgenerics/Library.java
@@ -0,0 +1,21 @@
+package genericsusage.withoutgenerics;
+
+import genericsusage.Book;
+
+import java.util.List;
+
+public class Library {
+
+    public Book getFirstBook(List books) {
+        if (books == null) {
+            throw new NullPointerException("Argument should not be null!");
+        }
+        if (books.isEmpty()) {
+            throw new IllegalArgumentException("Argument should not be empty!");
+        }
+        if (!(books.get(0) instanceof Book)) {
+            throw new ClassCastException("Not a book");
+        }
+        return (Book) books;
+    }
+}
