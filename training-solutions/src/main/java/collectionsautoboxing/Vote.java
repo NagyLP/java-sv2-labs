7f02be6b0a539dd440193ce27852b78cfef04a0c
NagyLP <nagy.peter@pr.hu>
2022-01-27 01:44:49 +0100

    Kollekciók: Autoboxing

--- training-solutions/src/main/java/collectionsautoboxing/Vote.java
+++ training-solutions/src/main/java/collectionsautoboxing/Vote.java
@@ -5,14 +5,26 @@ import java.util.Map;
 
 public class Vote {
 
-    public Map<VoteResult, Integer> getResult(Map<String, VoteResult> results) {
-        Map<VoteResult, Integer> finalResults = new HashMap<>();
-        for (VoteResult v : results.values()) {
-            if (!finalResults.containsKey(v)) {
-                finalResults.put(v, 0);
+    public Map<VoteResult, Integer> getResult(Map<String, VoteResult> voteResultMap) {
+        Map<VoteResult, Integer> results = new HashMap<>();
+        for (VoteResult item : voteResultMap.values()) {
+            if (!results.containsKey(item)) {
+                results.put(item, 0);
             }
-            finalResults.put(v, finalResults.get(v) + 1);
+            results.put(item, results.get(item) + 1);
         }
-        return finalResults;
+        return results;
     }
+
+//
+//    public Map<VoteResult, Integer> getResult(Map<String, VoteResult> results) {
+//        Map<VoteResult, Integer> finalResults = new HashMap<>();
+//        for (VoteResult v : results.values()) {
+//            if (!finalResults.containsKey(v)) {
+//                finalResults.put(v, 0);
+//            }
+//            finalResults.put(v, finalResults.get(v) + 1);
+//        }
+//        return finalResults;
+//    }
 }
