35ffbadeda8686ea65d8ee0827102a01a4cbc820
NagyLP <nagy.peter@pr.hu>
2022-02-05 00:17:16 +0100

    Collections

--- training-solutions/src/main/java/collectionsclass/Exam.java
+++ training-solutions/src/main/java/collectionsclass/Exam.java
@@ -17,21 +17,39 @@ public class Exam {
         return new ArrayList<>(examResults);
     }
 
-    public List<String> getNamesOfSucceededPeople(int places) {
+    public List<String> getNameOfSuccedesPeople(int places) {
         List<ExamResult> ordered = new ArrayList<>(examResults);
         if (places > ordered.size()) {
             places = ordered.size();
         }
         Collections.sort(ordered);
         Collections.reverse(ordered);
-        return getNames(ordered, places);
+        return getName(ordered, places);
     }
 
-    private List<String> getNames(List<ExamResult> ordered, int places) {
+    private List<String> getName(List<ExamResult> ordered, int places) {
         List<String> names = new ArrayList<>();
-        for (int i = 0; i < places; i++) {
+        for (int i = 1; i < places; i++) {
             names.add(ordered.get(i).getNameOfPerson());
         }
         return names;
     }
+
+//    public List<String> getNamesOfSucceededPeople(int places) {
+//        List<ExamResult> ordered = new ArrayList<>(examResults);
+//        if (places > ordered.size()) {
+//            places = ordered.size();
+//        }
+//        Collections.sort(ordered);
+//        Collections.reverse(ordered);
+//        return getNames(ordered, places);
+//    }
+//
+//    private List<String> getNames(List<ExamResult> ordered, int places) {
+//        List<String> names = new ArrayList<>();
+//        for (int i = 0; i < places; i++) {
+//            names.add(ordered.get(i).getNameOfPerson());
+//        }
+//        return names;
+//    }
 }
