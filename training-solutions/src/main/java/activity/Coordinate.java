b4d40de4e323956cc40a8d36491557a54fcb342f
NagyLP <nagy.peter@pr.hu>
2022-02-06 22:16:29 +0100

    Activity (SonarLint-True)

--- training-solutions/src/main/java/activity/Coordinate.java
+++ training-solutions/src/main/java/activity/Coordinate.java
@@ -6,7 +6,7 @@ public class Coordinate {
     private final double longitude;
 
     public Coordinate(double latitude, double longitude) {
-        switch (validatorCorrdinates(latitude, longitude)) {
+        switch (validatorCoordinates(latitude, longitude)) {
             case 1 -> throw new IllegalArgumentException("Fatal-Total-All ERROR - data: " + latitude + longitude + "\n Na persze...");
             case 2 -> throw new IllegalArgumentException("Syntax- ERROR -data: " + latitude + "\n(Latitude value [-90, 90])");
             case 3 -> throw new IllegalArgumentException("Syntax- ERROR -data: " + longitude + "\n(Longitude value [-180, 180])");
@@ -25,7 +25,7 @@ public class Coordinate {
         return longitude;
     }
 
-    private int validatorCorrdinates(double latitude, double longitude) {
+    private int validatorCoordinates(double latitude, double longitude) {
         if (latitude > 90 || latitude < -90 && longitude > 180 || longitude < -180) {
             return 1;
         }
