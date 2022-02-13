361c295bb1d72535c3e5bf741adc8b3d2e2d1ce6
NagyLP <nagy.peter@pr.hu>
2022-02-13 01:54:03 +0100

    SQL-Egyszerű lekérdezések

--- training-solutions/sql/queries/Video_Cserko Prof.sql
+++ training-solutions/sql/queries/Video_Cserko Prof.sql
@@ -1,5 +1,3 @@
--- SELECT - FROM
--- 
 -- CREATE TABLE Customers (
 -- id INT,
 -- name VARCHAR(100),
@@ -33,11 +31,11 @@
 -- PRIMARY KEY (id)
 -- );
 -- 
-
+-- 
 -- CREATE TABLE Course (
 -- id INT NOT NULL AUTO_INCREMENT,
 -- name VARCHAR(50),
--- period INT,
+-- period VARCHAR(50),
 -- level VARCHAR(50),
 -- price INT,
 -- PRIMARY KEY (id)
@@ -50,17 +48,20 @@
 -- email VARCHAR(50),
 -- PRIMARY KEY(ID)
 -- );
-
+-- 
 -- SELECT*FROM teacher
 -- WHERE city = 'Debrecen';
-
+-- 
 -- SELECT * FROM student
 -- WHERE
 -- 		city = 'Budapest'
 -- 		AND
 -- 		email LIKE '%gmail%';
-
-
+-- 
+-- 
+-- 
+-- -AND, OR, NOT - FELTÉTELEK
+-- 
 -- SELECT * FROM teacher
 -- WHERE
 -- 	id > 1
@@ -68,8 +69,8 @@
 -- 	city = 'budapest'
 -- 	OR
 -- 	city = 'Debrecen'
--- 	;
-
+-- ;
+-- 
 -- SELECT * FROM student
 -- WHERE
 -- 	id > 1
@@ -77,4 +78,107 @@
 -- 	city = 'Budapest'
 -- 	OR
 -- 	city = 'Debrecen'
+-- ;
+-- 
+-- 
+-- ORDER BY - RENDEZÉS
+-- 
+-- SELECT * FROM teacher 
+-- 	WHERE
+-- 		id > 1
+-- 		AND
+-- 		city = 'Budapest'
+-- 		OR
+-- 		city = 'Debrecen'
+-- 	ORDER BY NAME
+-- ;
+-- 
+-- SELECT * FROM student
+-- 	WHERE
+-- 		id > 1
+-- 		and
+-- 		city = 'budapest'
+-- 		or
+-- 		city = 'debrecen'
+-- 	ORDER BY email
+-- ;
 
+
+ 
+-- MIN & MAX értékek keresése
+-- 
+-- SELECT MAX(credit) AS MaxCredit
+-- 	FROM student 
+-- 	WHERE
+-- 	city = 'budapest'
+-- ;
+
+
+
+-- COUNT, AVG, SUM - Meszámlálás, átlagolás, és összegzés
+-- 
+-- SELECT SUM(credit) AS SumCredit
+-- 	FROM student
+-- 	WHERE not
+-- 	city = 'budapest'
+-- ;
+-- 
+-- SELECT COUNT(id) AS NumTeachers
+-- 	FROM teacher
+-- 	WHERE city = 'budapest'
+-- ;
+-- 
+-- SELECT COUNT(id) AS Actual
+-- 	FROM course
+-- 	WHERE period = 'active'
+-- ;
+
+-- SELECT--  AVG(credit) AS AvgCredit
+-- 	FROM student
+-- 	WHERE city = 'budapest'
+-- ;
+
+
+
+-- GROUP BY - Csoportosítás
+
+/* SÉMA:
+SELECT column_name(s)   	// kiválaszt oszlopnevek
+FROM table_name				// melyik táblában
+WHERE condition				// szűrés kondíciója, alapja
+GROUP BY column_name(s)		// csoportosít adat
+ORDER BY column_name(s);	// rendez adat
+*/
+
+-- SELECT count(id) AS 'Tanulók szám a?', city
+-- FROM student
+-- GROUP BY city
+-- ;
+
+
+
+-- LIKE - keresés hasonlóság alapján
+
+/* SELECT column1, column2, ...
+FROM table_name
+WHERE columnN LIKE pattern;
+*/
+
+-- SELECT SUM(credit)
+-- FROM student
+-- WHERE email LIKE '%gmail%'
+-- ;
+
+
+
+-- INTO -----
+-- 
+-- INSERT INTO student
+-- 	VALUES
+-- 	(6, 'Robert Downey Jr.', 'Manhattan', 'robdow@jr.org'),
+-- 	(7, 'Jon Favreau', 'Marie Byrd-föld', 'jf@jf.com'),
+-- 	(8, 'Gwyneth Paltrow', 'Északi-sark', 'gp@gp.gp');
+-- 
+-- 
+-- 
+-- 
