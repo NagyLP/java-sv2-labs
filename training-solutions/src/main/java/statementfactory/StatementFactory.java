651dfeb42eab28528480cc25073faffb0d2b8535
NagyLP <nagy.peter@pr.hu>
2022-02-18 21:55:40 +0100

    ActivityTracker

--- training-solutions/src/main/java/statementfactory/StatementFactory.java
+++ training-solutions/src/main/java/statementfactory/StatementFactory.java
@@ -1,5 +1,6 @@
 package statementfactory;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,26 +11,48 @@ public class StatementFactory {
     private StatementFactory() {
     }
 
-    public static PreparedStatement getParameterizedStatement(Connection connection, String stringSql, Object... values) throws SQLException {
+    @SuppressWarnings("java:S2095")
+    public static PreparedStatement getParameterizedStatement
+            (Connection connection, String stringSql, Object... values)
+            throws SQLException {
+
         PreparedStatement stmt = connection.prepareStatement(stringSql);
         for (int i = 1; i <= values.length; i++) {
             Object parameters = values[i - 1];
+
             if (parameters instanceof String s) {
                 stmt.setString(i, s);
+
             } else if (parameters instanceof Integer j) {
                 stmt.setInt(i, j);
+
             } else if (parameters instanceof Long l) {
                 stmt.setLong(i, l);
+
             } else if (parameters instanceof Double d) {
                 stmt.setDouble(i, d);
+
             } else if (parameters instanceof LocalDate date) {
                 stmt.setDate(i, Date.valueOf(date));
+
             } else if (parameters instanceof LocalDateTime dateTime) {
                 stmt.setTimestamp(i, Timestamp.valueOf(dateTime));
+
             } else if (parameters instanceof LocalTime time) {
                 stmt.setTime(i, Time.valueOf(time));
+
+            } else {
+                throw new IllegalArgumentException(String.format("Invalid parameter type at SQL-index %d: %s",
+                        i, parameters.getClass().getName()));
             }
         }
         return stmt;
+    }
+
+    public static Connection continuousConnection(DataSource dataSource) throws SQLException {
+        @SuppressWarnings("java:S2095")
+        Connection connection = dataSource.getConnection();
+        connection.setAutoCommit(false);
+        return connection;
     }
 }
