307281b2d04ba4a16bf205978821586a67e76b2a
NagyLP <nagy.peter@pr.hu>
2022-02-18 13:39:14 +0100

    ActivityTracker

--- training-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
+++ training-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
@@ -9,12 +9,14 @@ public class ActivityTrackerMain {
 
     public static void main(String[] args) {
         ActivityTrackerMain main = new ActivityTrackerMain();
-        main.connectMariaDataSource();
-
-
+        try {
+            main.connectToCleanMariaDb();
+        } catch (SQLException sqle) {
+            throw new IllegalStateException("Connection ERROR");
+        }
     }
 
-    private void connectMariaDataSource() throws SQLException {
+    private void connectToCleanMariaDb() throws SQLException {
         MariaDbDataSource dataSource = new MariaDbDataSource();
         dataSource.setUrl("jdbc:mariadb://localhost:3306/movies-actors?useUnicode=true");
         dataSource.setUser("activitytracker");
