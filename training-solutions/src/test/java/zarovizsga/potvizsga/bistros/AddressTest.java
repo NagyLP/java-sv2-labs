6bcc7174fb6e1523cfb83f314359e81072e2c3f4
NagyLP <nagy.peter@pr.hu>
2022-03-04 16:52:13 +0100

    Training: Záró-, pótvizsga

---
+++ training-solutions/src/test/java/zarovizsga/potvizsga/bistros/AddressTest.java
@@ -0,0 +1,18 @@
+//package zarovizsga.potvizsga.bistros;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class AddressTest {
+//
+//
+//    @Test
+//    void createAddressTest(){
+//        Address address = new Address("Kossuth",18);
+//
+//        assertEquals("Kossuth",address.getStreet());
+//        assertEquals(18,address.getNumber());
+//    }
+//
+//}
