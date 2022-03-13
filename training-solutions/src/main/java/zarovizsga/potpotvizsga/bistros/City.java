0202ec24a09d32ff843889c5cf1215ed58c5fa63
NagyLP <nagy.peter@pr.hu>
2022-03-13 22:48:01 +0100

    OO - Bisztró

---
+++ training-solutions/src/main/java/zarovizsga/potpotvizsga/bistros/City.java
@@ -0,0 +1,17 @@
+package zarovizsga.potpotvizsga.bistros;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class City {
+
+    private final List<Bistro> bistros = new ArrayList<>();
+
+    public void addBistro(Bistro bistro) {
+        bistros.add(bistro);
+    }
+
+    public List<Bistro> getBistros() {
+        return bistros;
+    }
+}
