ab49aba2b47fdd6009b44ce0c852d6fe6b717550
NagyLP <nagy.peter@pr.hu>
2022-03-13 20:55:25 +0100

    Pótpótzáróv.

--- training-solutions/src/main/java/A_Training/Main.java
+++ training-solutions/src/main/java/A_Training/Main.java
@@ -1,23 +1,31 @@
 package A_Training;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        while (scan.hasNextLine()) {
-            int anInt = scan.nextInt();
-            double aDouble = scan.nextDouble();
-            scan.nextLine();
-            String aString = scan.nextLine();
+        Scanner scanIn = new Scanner(System.in);
 
-            System.out.println(String.format(
-                    "String: %s%n" +
-                            "Double: %s%n" +
-                            "Int: %d",
-                    aString, aDouble, anInt));
+        String regex = "(\\s|^)([a-z]+)(\\s+\\2)+(?=(?:\\s|$))";
+        Pattern pattern = Pattern.compile(
+                regex, Pattern.CASE_INSENSITIVE);
+        String numSentences = scanIn.nextLine();
+
+        while (!numSentences.isEmpty()) {
+            String input = scanIn.nextLine();
+            Matcher match = pattern.matcher(input);
+            boolean findMatch = true;
+
+            while (match.find()) {
+                input = match.replaceAll("$1");
+                findMatch = false;
+            }
+            System.out.println(input);
         }
+        scanIn.close();
     }
 }
 
---
+++ training-solutions/src/main/java/zarovizsga/potpotvizsga/numbers/RisingNumbers.java
@@ -0,0 +1,36 @@
+package zarovizsga.potpotvizsga.numbers;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class RisingNumbers {
+
+    public int getNumberOfSixDigitRisingNumbers(List<Integer> numbers) {
+        validateListIsBlank(numbers);
+
+    }
+
+    // JCF -Keretrendszer- Kollekciók
+
+    private void validateDigitsIsDifferent(List<Integer> digits) {
+        if (digits.size() != new HashSet<>(digits).size())
+            throw new IllegalArgumentException("Digits is not different!");
+    }
+
+    // HIBAÜZENET KISZERVEZÉSE (String switch-be?)
+
+    private void validateNumbersIsSixDigits(int number) {
+        if (number < 100_000 || number > 999_999)
+            throw new IllegalArgumentException("There are not six digits!");
+    }
+
+    private void validateListIsBlank(List<Integer> numbers) {
+        if (numbers == null) {
+            throw new IllegalArgumentException("There are no numbers!");
+        }
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException("There are no numbers!");
+        }
+    }
+
+}
