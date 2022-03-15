91e7db13f027a24638d32e304441010ab48d85e1
NagyLP <nagy.peter@pr.hu>
2022-03-15 17:28:07 +0100

    HackerRank - Primitívek értékei

--- training-solutions/src/main/java/A_Training/Main.java
+++ training-solutions/src/main/java/A_Training/Main.java
@@ -1,31 +1,72 @@
 package A_Training;
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanIn = new Scanner(System.in);
+//        Scanner scanIn = new Scanner(System.in);
+//
+//        String regex = "(\\s|^)([a-z]+)(\\s+\\2)+(?=(?:\\s|$))";
+//        Pattern pattern = Pattern.compile(
+//                regex, Pattern.CASE_INSENSITIVE);
+//        String numSentences = scanIn.nextLine();
+//
+//        while (!numSentences.isEmpty()) {
+//            String input = scanIn.nextLine();
+//            Matcher match = pattern.matcher(input);
+//            boolean findMatch = true;
+//
+//            while (match.find()) {
+//                input = match.replaceAll("$1");
+//                findMatch = false;
+//            }
+//            System.out.println(input);
+//        }
+//        scanIn.close();
 
-        String regex = "(\\s|^)([a-z]+)(\\s+\\2)+(?=(?:\\s|$))";
-        Pattern pattern = Pattern.compile(
-                regex, Pattern.CASE_INSENSITIVE);
-        String numSentences = scanIn.nextLine();
+//        Scanner scan = new Scanner(System.in);
+//        Main mainSolution = new Main();
+//
+//        int round = scan.nextInt();
+//        for(int i=0; i<round; i++) {
+//            int a = scan.nextInt();
+//            int b = scan.nextInt();
+//            int n = scan.nextInt();
+//
+//            mainSolution.innerFor(a, b, n);
+//
+//            System.out.println();
+//        }
+//    }
+//
+//    private void innerFor(int a, int b, int n) {
+//        for(int j=0; j<n; j++) {
+//            a += b * (int) Math.pow(2, j);
+//            System.out.print(a + " ");
+//        }
 
-        while (!numSentences.isEmpty()) {
-            String input = scanIn.nextLine();
-            Matcher match = pattern.matcher(input);
-            boolean findMatch = true;
+//        Scanner scan = new Scanner(System.in);
+//        int data = scan.nextInt();
+//        for (int i = 0; i < data; i++) {
+//            try {
+//                if (data >= Byte.MIN_VALUE
+//                        && data <= Byte.MAX_VALUE)
+//                    System.out.println("* byte");
+//                if (data >= Short.MIN_VALUE
+//                        && data <= Short.MAX_VALUE)
+//                    System.out.println("* short");
+//                if (data >= Integer.MIN_VALUE
+//                        && data <= Integer.MAX_VALUE)
+//                    System.out.println("* int");
+//                if (data >= (Long.MIN_VALUE)
+//                        && data <= (Math.pow(2, 63) - 1))
+//                    System.out.println("* long");
+//
+//            } catch (Exception ioe) {
+//                System.out.println(scan.next() + " can be fitted in:");
+//            }
+//        }
 
-            while (match.find()) {
-                input = match.replaceAll("$1");
-                findMatch = false;
-            }
-            System.out.println(input);
-        }
-        scanIn.close();
+
     }
 }
 
