0760db81634bc2382c6297fa90af7c6221500eae
NagyLP <nagy.peter@pr.hu>
2022-03-21 17:01:09 +0100

    Repeat

---
+++ training-solutions/src/main/resources/db/migration/V1__dino.sql
@@ -0,0 +1,6 @@
+create TABLE dinosaur (breed VARCHAR(255) PRIMARY KEY, expected INT, actual INT);
+insert into dinosaur (breed, expected, actual) values ('Tyrannosaurus Rex', 2, 2);
+insert into dinosaur (breed, expected, actual) values ('Stegosaurus', 4, 4);
+insert into dinosaur (breed, expected, actual) values ('Maiasaurus', 21, 22);
+insert into dinosaur (breed, expected, actual) values ('Hypsilophodontida', 33, 34);
+insert into dinosaur (breed, expected, actual) values ('Velociraptor', 8, 37);
