9b38df5c28d2753cfd2fcbb0018c34d8eba17c16
NagyLP <nagy.peter@pr.hu>
2022-03-21 19:56:00 +0100

    WriteFile: Concat

---
+++ training-solutions/src/main/java/kepesitovizsga/phonebook/Phonebook.java
@@ -0,0 +1,33 @@
+package kepesitovizsga.phonebook;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Phonebook {
+
+    public void exportPhonebook(TreeMap<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException("Contacts OR Output is null.");
+        }
+
+        try {
+            Files.write(Path.of(output), entryFromLine(contacts));
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cant write file." + ioe);
+        }
+    }
+
+    private List<String> entryFromLine(TreeMap<String, String> contacts) {
+        List<String> entryFromLine = new ArrayList<>();
+        for (Map.Entry<String, String> entry : contacts.entrySet()) {
+            entryFromLine.add(entry.getKey().concat(": ").concat(entry.getValue()));
+        }
+        return entryFromLine;
+    }
+}
